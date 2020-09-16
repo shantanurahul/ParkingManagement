f155f70e20ff8205c1696566680d8208f41670c4
Shantanu <shantanusrivastv-sw@yahoo.co.in>
2020-09-16 01:51:14 +0100

    Added few ignore files and completed Long Stay Calc

---
+++ ParkingManagement.Tests/LongStayCalculatorTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+
+
+namespace ParkingManagement.Tests
+{
+    [TestFixture]
+    public class LongStayCalculatorTest
+    {
+        private LongStayCalculator _sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _sut = new LongStayCalculator();
+        }
+
+        [TestCase("09/07/2017 07:50:00 ", "09/09/2017 05:20:00 ", 22.50)]
+        [TestCase("09/07/2017 07:50:00 ", "09/07/2017 07:50:01 ", 7.50)]
+        public void GetChrgeableMidDaysTest(DateTime entryDate, DateTime exitDate, decimal expected)
+        {
+            decimal res = _sut.ParkingCharge(entryDate, exitDate);
+            Assert.AreEqual(expected, res);
+        }
+    }
+}
---
+++ ParkingManagement/LongStayCalculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ParkingManagement
+{
+    public class LongStayCalculator
+    {
+        private const decimal PerDayParkingFee = 7.50m;
+
+        public decimal ParkingCharge(DateTime entryTime, DateTime exitTime)
+        {
+            if (!(entryTime > exitTime))
+            {
+                double chargeableDays = GetChargeableDays(entryTime, exitTime);
+                decimal totalCharge = Convert.ToDecimal(chargeableDays) * PerDayParkingFee;
+                return decimal.Round(totalCharge, 2);
+            }
+
+            throw new ArgumentException("Entry Date cannot be greate than Exit date");
+        }
+
+        private double GetChargeableDays(DateTime entryTime, DateTime exitTime)
+        {
+            // Car is Parked for over a day
+            if (exitTime.Date > entryTime.Date)
+            {
+                // Rouding up to whole days adding a day
+                return Math.Ceiling((exitTime - entryTime).TotalDays + 1d);
+            }
+            else
+            {
+                return 1d;
+            }
+                
+        }
+    }
+}
