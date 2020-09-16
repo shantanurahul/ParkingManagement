66fdc6b53c2e6e204be045b6ba612077f1d00bd7
Shantanu <shantanusrivastv-sw@yahoo.co.in>
2020-09-16 01:55:27 +0100

    Added Few more test cases

--- ParkingManagement.Tests/LongStayCalculatorTest.cs
+++ ParkingManagement.Tests/LongStayCalculatorTest.cs
@@ -15,8 +15,10 @@ namespace ParkingManagement.Tests
             _sut = new LongStayCalculator();
         }
 
-        [TestCase("09/07/2017 07:50:00 ", "09/09/2017 05:20:00 ", 22.50)]
-        [TestCase("09/07/2017 07:50:00 ", "09/07/2017 07:50:01 ", 7.50)]
+        [TestCase("09/07/2017 07:50:00 ", "09/09/2017 05:20:00 ", 22.50)] //Orignal TestCase
+        [TestCase("09/07/2017 07:50:00 ", "09/07/2017 07:50:01 ", 7.50)] //Same Day exit
+        [TestCase("09/07/2017 07:50:00 ", "09/08/2017 00:00:01 ", 15.00)] //Edge Case next day
+        [TestCase("09/07/2017 00:00:00 ", "09/07/2017 00:00:01 ", 7.50)] //Edge Case same day
         public void GetChrgeableMidDaysTest(DateTime entryDate, DateTime exitDate, decimal expected)
         {
             decimal res = _sut.ParkingCharge(entryDate, exitDate);
